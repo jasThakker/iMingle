a580dfd603dcf96bc6b560ff4e7434b037280611
jasThakker <jthakker@ucsd.edu>
2025-11-08 02:51:41 -0800

    Initial Commit

---
+++ iMingle/iMingleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMingleApp.swift
+//  iMingle
+//
+//  Created by Jas Thakker on 08/11/25.
+//
+
+import SwiftUI
+
+@main
+struct iMingleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iMingleTests/iMingleTests.swift
@@ -0,0 +1,17 @@
+//
+//  iMingleTests.swift
+//  iMingleTests
+//
+//  Created by Jas Thakker on 08/11/25.
+//
+
+import Testing
+@testable import iMingle
+
+struct iMingleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
